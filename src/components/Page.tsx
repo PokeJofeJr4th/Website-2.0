060525b764f3e0b342c34f7b6bbde6d188f86a17
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2025-03-27 13:13:18 -0400

    stylesheet

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 
+import "./main.css";
+
 export default function Page({
   title,
   children,
-}: React.PropsWithChildren<{ title: string }>) {
+}: React.PropsWithChildren<{ title?: string }>) {
   return (
     <html lang="en">
       <head>
+        <title>{title} | Abigail JC</title>
         <meta charSet="utf-8" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <meta name="viewport" content="width=device-width" />
       </head>
       <body>
-        <h1>{title}</h1>
-        {children}
+        <nav>
+          <a href="/" className="button">
+            Home
+          </a>
+          <a href="/projects" className="button">
+            Projects
+          </a>
+          <a href="/posts" className="button">
+            Posts
+          </a>
+          <a href="/tools" className="button">
+            Tools
+          </a>
+        </nav>
+        <section>
+          {title && <h1>{title}</h1>}
+          {children}
+        </section>
       </body>
     </html>
   );
